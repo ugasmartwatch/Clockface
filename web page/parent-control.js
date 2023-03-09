65942beed66fc3d26d1e64f4e91c418d8ec3644a
mtd90955 <82740685+mtd90955@users.noreply.github.com>
2023-03-08 19:48:14 -0500

    Update parent-control.js

--- web page/parent-control.js
+++ web page/parent-control.js
@@ -1,48 +1,54 @@
-var message = document.getElementById("detail").value;
-var time = document.getElementById("hour").value * 3600000) + (document.getElementById("minute").value * 60000;
-// var BANGLE_CODE = `
-// Bangle.loadWidgets();
-// Bangle.drawWidgets();
-// require("sched").getAlarms();
-// require("sched").newDefaultAlarm();
-// require("sched").setAlarm("myalarm", {
-//   t: (2 * 3600000) + (16 * 60000),
-// });
-// require("sched").reload();
-// Bangle.buzz();
-// Bangle.setLCDPower(1);
-// E.showMessage(message).then(function(v) {
-//   if (v) print("'Yes' chosen");
-//   else print("'No' chosen");
-// });
-// `;
+const message = document.getElementById("detail");
+const hour = document.getElementById("hour");
+const min = document.getElementById("minute");
+// const buzz = false;
+// const beep = false;
+// const vibrate = false;
+// const reminderType = document.getElementById("type");
 
-var BANGLE_CODE = `
-Bangle.loadWidgets();
-Bangle.drawWidgets();
-require("sched").getAlarms();
-require("sched").newDefaultAlarm();
-require("sched").setAlarm("myalarm", {
-  msg: message,
-  t: time,
-  rp: 
-});
-require("sched").reload();
-Bangle.buzz();
-Bangle.setLCDPower(1);
-E.showMessage(message).then(function(v) {
-  if (v) print("'Yes' chosen");
-  else print("'No' chosen");
-});
-`;
+
 
 var connection;
 document.getElementById("save").addEventListener("click", function() {
+  
   // disconnect if connected already
   if (connection) {
     connection.close();
     connection = undefined;
   }
+//
+
+  // const msgValue = message.value;
+  // const msgObj = {value: ""};
+  // msgObj.value = msgValue;
+  // const msgJSON = JSON.stringify(msgObj);
+  // const time = JSON.stringify(hour.value * 3600000 + min.value * 60000);
+//   function loadAlarm() {
+//     var alarms = require("sched").getAlarms();
+//     var alarm = require("sched").newDefaultAlarm();
+//     alarm.msg = JSON.stringify(message.value);
+//     alarm.time = hour.value * 3600000 + min.value * 60000;
+//     alarms.push(alarm);
+//     require("sched").setAlarm(alarms);
+//     require("sched").reload();
+    
+// Bangle.buzz();
+// Bangle.setLCDPower(1);
+//   }
+  var BANGLE_CODE = `
+    Bangle.loadWidgets();
+    Bangle.drawWidgets();
+    require("sched").getAlarms();
+    require("sched").newDefaultAlarm();
+    require("sched").setAlarm("myalarm", () {
+      msg: "Hello",
+      t: 3*3600000
+    });
+    require("sched").reload();
+    Bangle.buzz();
+    Bangle.setLCDPower(1);
+`;
+
 
   // Connect
   Puck.connect(function(c) {
@@ -52,13 +58,41 @@ document.getElementById("save").addEventListener("click", function() {
     }
     connection = c;
     // First, reset the Bangle
-    connection.write("reset();\n", function() {
-      // Wait for it to reset itself
-      setTimeout(function() {
-        // Now upload our code to it
-        connection.write(BANGLE_CODE);
-      }, 1000);
-    });
+    connection.write("reset();\n", function () {
+        // Wait for it to reset itself
+        setTimeout(function () {
+          // Now upload our code to it
+          connection.write(BANGLE_CODE);
+        }, 1000);
+      });
 
   });
 });
+
+// create a button to connect the watch first
+// maybe load the alarms from the watch to the web page
+// if alarms are loaded, maybe create a button to delete alarm
+
+// create a button to add new alarm
+// create a button to save it into the watch
+// hide the alarm icon on watch bar
+// maybe remove the alarm app from the watch so the kids could not modify the alarm
+// 
+
+// const list = document.querySelector('#myUL');
+// const input = document.querySelector('#usrInput');
+// const button = document.querySelector('#addBtn');
+// button.addEventListener('click', () => {
+//     const myItem = input.value;
+//     input.value = '';
+//     const listItem = document.createElement('li');
+//     listItem.textContent = myItem;
+//     const listBtn = document.createElement('button');
+//     listBtn.textContent = "Delete";
+//     listItem.appendChild(listBtn);
+//     list.appendChild(listItem);
+//     listBtn.addEventListener('click', () => {
+//         list.removeChild(listItem);
+//     });
+//     input.focus();
+// });
